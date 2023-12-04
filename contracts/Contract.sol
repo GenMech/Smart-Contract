0194fb009df5d3c25fb1312e167d1cdb61d20f62
TomarJatin <jt24072002@gmail.com>
2023-12-05 01:56:16 +0530

    contract updated

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -17,6 +17,7 @@ contract MyContract {
         string manufactuerTimestamp;
         string retailer;
         string[] customers;
+        address myAdd;
     }
 
     struct CustomerObj {
@@ -24,16 +25,25 @@ contract MyContract {
         string phone;
         string[] code;
         bool isValue;
+        address myAdd;
+    }
+
+    struct ManufactureObj{
+        address myAdd;
+        string name;
+        string location;
     }
 
     struct RetailerObj {
         string name;
         string location;
+        address myAdd;
     }
 
     mapping(string => CodeObj) public codeArr;
     mapping(string => CustomerObj) public customerArr;
     mapping(string => RetailerObj) public retailerArr;
+    mapping(address => ManufactureObj) public manufactureArr;
 
     modifier onlyOwner {
         require(msg.sender == owner, "Not the owner");
@@ -41,13 +51,12 @@ contract MyContract {
     }
 
     function createCode(
+        address manufactureAdd,
         string memory _code,
         string memory _brand,
         string memory _model,
         uint _status,
         string memory _description,
-        string memory _manufactuerName,
-        string memory _manufactuerLocation,
         string memory _manufactuerTimestamp
     ) public returns (uint) {
         CodeObj memory newCode;
@@ -55,9 +64,10 @@ contract MyContract {
         newCode.model = _model;
         newCode.status = _status;
         newCode.description = _description;
-        newCode.manufactuerName = _manufactuerName;
-        newCode.manufactuerLocation = _manufactuerLocation;
+        newCode.manufactuerName = manufactureArr[manufactureAdd].name;
+        newCode.manufactuerLocation =  manufactureArr[manufactureAdd].location;
         newCode.manufactuerTimestamp = _manufactuerTimestamp;
+        newCode.myAdd = manufactureAdd;
         codeArr[_code] = newCode;
         return 1;
     }
@@ -74,16 +84,20 @@ contract MyContract {
         );
     }
 
-    function getOwnedCodeDetails(string memory _code) public view returns (string memory, string memory) {
-        return (retailerArr[codeArr[_code].retailer].name, retailerArr[codeArr[_code].retailer].location);
+    function getOwnedCodeDetails(string memory _code) public view returns (string memory, string memory, address) {
+        return (retailerArr[codeArr[_code].retailer].name, retailerArr[codeArr[_code].retailer].location, retailerArr[codeArr[_code].retailer].myAdd);
     }
 
-    function addRetailerToCode(string memory _code, string memory _hashedEmailRetailer) public returns (uint) {
+    function addRetailerToCode(address currAdd, string memory _code, string memory _hashedEmailRetailer) public returns (uint) {
+        if(codeArr[_code].myAdd != currAdd){
+            return 0;
+        }
         codeArr[_code].retailer = _hashedEmailRetailer;
+        codeArr[_code].myAdd = retailerArr[_hashedEmailRetailer].myAdd;
         return 1;
     }
 
-    function createCustomer(string memory _hashedEmail, string memory _name, string memory _phone) public returns (bool) {
+    function createCustomer(address currAdd, string memory _hashedEmail, string memory _name, string memory _phone) public returns (bool) {
         if (customerArr[_hashedEmail].isValue) {
             return false;
         }
@@ -91,24 +105,35 @@ contract MyContract {
         newCustomer.name = _name;
         newCustomer.phone = _phone;
         newCustomer.isValue = true;
+        newCustomer.myAdd = currAdd;
         customerArr[_hashedEmail] = newCustomer;
         return true;
     }
 
-    function getCustomerDetails(string memory _code) public view returns (string memory, string memory) {
-        return (customerArr[_code].name, customerArr[_code].phone);
+    function getCustomerDetails(string memory _code) public view returns (string memory, string memory, address) {
+        return (customerArr[_code].name, customerArr[_code].phone, customerArr[_code].myAdd);
     }
 
-    function createRetailer(string memory _hashedEmail, string memory _retailerName, string memory _retailerLocation) public returns (uint) {
+    function createRetailer(address currAdd, string memory _hashedEmail, string memory _retailerName, string memory _retailerLocation) public returns (uint) {
         RetailerObj memory newRetailer;
         newRetailer.name = _retailerName;
         newRetailer.location = _retailerLocation;
+        newRetailer.myAdd = currAdd;
         retailerArr[_hashedEmail] = newRetailer;
         return 1;
     }
 
-    function getRetailerDetails(string memory _code) public view returns (string memory, string memory) {
-        return (retailerArr[_code].name, retailerArr[_code].location);
+    function createManufacture(address manuAdd, string memory name, string memory location) public returns (uint) {
+        ManufactureObj memory newManufacture;
+        newManufacture.name = name;
+        newManufacture.location = location;
+        newManufacture.myAdd = manuAdd;
+        manufactureArr[manuAdd] = newManufacture;
+        return 1;
+    }
+
+    function getRetailerDetails(string memory _code) public view returns (string memory, string memory, address) {
+        return (retailerArr[_code].name, retailerArr[_code].location, retailerArr[_code].myAdd);
     }
 
     function reportStolen(string memory _code, string memory _customer) public returns (bool) {
@@ -124,10 +149,13 @@ contract MyContract {
         return false;
     }
 
-    function changeOwner(string memory _code, string memory _oldCustomer, string memory _newCustomer) public returns (bool) {
+    function changeOwner(address currAdd, string memory _code, string memory _oldCustomer, string memory _newCustomer) public returns (bool) {
         uint i;
         bool flag = false;
         CodeObj storage product = codeArr[_code];
+        if(product.myAdd != currAdd){
+            return false;
+        }
 
         if (customerArr[_oldCustomer].isValue && customerArr[_newCustomer].isValue) {
             for (i = 0; i < customerArr[_oldCustomer].code.length; i++) {
@@ -141,6 +169,7 @@ contract MyContract {
                 for (i = 0; i < product.customers.length; i++) {
                     if (compareStrings(product.customers[i], _oldCustomer)) {
                         codeArr[_code].customers[i] = _newCustomer;
+                        codeArr[_code].myAdd = customerArr[_newCustomer].myAdd;
                         break;
                     }
                 }
@@ -164,10 +193,15 @@ contract MyContract {
         return false;
     }
 
-    function initialOwner(string memory _code, string memory _retailer, string memory _customer) public returns (bool) {
+    function initialOwner(address currAdd, string memory _code, string memory _retailer, string memory _customer) public returns (bool) {
+        if(currAdd != codeArr[_code].myAdd){
+            return false;
+        }
+
         if (compareStrings(codeArr[_code].retailer, _retailer) && customerArr[_customer].isValue) {
             codeArr[_code].customers.push(_customer);
             codeArr[_code].status = 1;
+            codeArr[_code].myAdd = customerArr[_customer].myAdd;
             uint len = customerArr[_customer].code.length;
             if (len == 0) {
                 customerArr[_customer].code.push(_code);
